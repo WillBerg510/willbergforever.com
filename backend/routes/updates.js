42adb94c93dca76106339d12b18349073e980d20
Will Berg <willberg510@gmail.com>
2026-02-14 14:07:05 -0500

    Fix multiple responses being sent for updates.one/:id delete

--- backend/routes/updates.js
+++ backend/routes/updates.js
@@ -48,7 +48,9 @@ router.delete("/one/:id", auth, async (req, res) => {
     if (!update) {
       res.status(404).json({error: "Update with that ID does not exist"});
     }
-    res.status(204).json({message: "Update deleted"});
+    else {
+      res.status(204).json({message: "Update deleted"});
+    }
   } catch (err) {
     res.status(500).json({error: "Error deleting update"});
   }
